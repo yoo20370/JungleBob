fb58df205da54f9bf45f1eab7d4f64025c998dbe
yoo20370 <email@email.com>
2024-03-19 16:20:00 +0900

    회원가입 기능 추가 # 프론트엔드 프레임워크 적용 전

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Flask 
-from flask import Flask, render_template, jsonify, request, redirect
+from flask import Flask, render_template, jsonify, request, redirect, session
 app = Flask(__name__)
 
 # pyjwt
@@ -13,31 +13,48 @@ db = client.jungleBob
 # jwt를 위한 비밀키
 SECRET_KEY = "jungleBob"
 
-@app.route("/")
+# 메뉴를 출력 
+@app.route("/today")
 def home() :
     return render_template('index.html')
 
-@app.route("/login", methods=['POST'])
-def loginCheck() :
-    userId = request.form['userId']
-    userPw = request.form['userPw']
-    # 1. 토큰이 함께 전송 됐는지 확인한다. 
-    # 1-1. 토큰이 있는 경우 DB에 접근할 필요 X
-    # 1-2. 토큰이 없는 경우 로그인 아이디와 비밀번호를 확인하고 토큰을 발행해서 클라이언트에 전달한다. ( 클라이언트에서 쿠키나 로컬 저장소에 저장한다. )
-    userData = db.users.find_one({"id":userId},{"_id":False})
+#################################### 회원가입 
 
-    if userId == userData['id'] and userPw == userData['password'] :
+# 회원가입 화면 출력 
+@app.route("/signIn", methods=['GET'])
+def signInGet() : 
+    return render_template("signin.html")
 
-        payload = {
-            "id":userData['id'],
-            "pw":userData['password']
-        }
-        token =  jwt.encode(payload , SECRET_KEY , "HS256")
-        return jsonify({"msg":"create token", "token": token})        
-
-    # 토큰으로 로그인했는데 로그인 창으로 오는 경우 redirect로 홈으로 이동시킨다. 
-    return redirect('http://localhost:5000/')
+# 회원가입 요청 
+@app.route("/signIn", methods=['POST'])
+def signInPost() :
+    userId = request.form['signinID'].strip()
+    userPw = request.form['signinPW'].strip()
+    userName = request.form['signinName'].strip()
     
+    userData = db.users.find_one({"id":userId})
+    
+    if userData == None :
+        result = db.users.insert_one({"id":userId, "pw":userPw, "name":userName});
+        print(result)
+        return jsonify({"msg":"signin success"})
+    else :
+        return jsonify({"msg":"Fail"})
+        
+#####################################
+
+#################################### 로그인 
+# 로그인 창 띄우기 
+@app.route("/login", methods=['GET'])
+def loginGet() :
+    pass
+
+# 로그인 시도 
+@app.route("/login", methods=['POST'])
+def loginPost() :
+    pass
+
+#####################################
 
 if __name__ == "__main__" :
     app.run("0.0.0.0", port=5000, debug=True)
