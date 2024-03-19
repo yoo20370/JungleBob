0da0f1a08232c0febeb21ca0ba4e7f75d4b4b28b
yunnn426 <lucia_hwang@naver.com>
2024-03-19 16:52:18 +0900

    hyk 병합

--- app.py
+++ app.py
@@ -10,5 +10,15 @@ db = client.jungleBob
 def home() :
     return render_template('index.html')
 
+from datetime import datetime
+@app.route("/today", methods=['GET'])
+def today() : 
+    date = ""
+    date += str(datetime.today().year) + "-"
+    date += str(datetime.today().month) + "-"
+    date += str(datetime.today().day)
+
+    return render_template('today.html', template_date = date)
+
 if __name__ == "__main__" :
-    app.run("0.0.0.0", port=5000, debug=True)
+    app.run("0.0.0.0", port=5001, debug=True)
