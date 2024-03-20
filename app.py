2e2acccd8147c49feca8f9fb4de6378a0717fa50
yunnn426 <lucia_hwang@naver.com>
2024-03-20 10:30:17 +0900

    이름판 화면 be 구현중

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ db = client.jungleBob
 # jwt를 위한 비밀키
 SECRET_KEY = "jungleBob"
 
+# Port 번호
+PORT = 5001
+
 # 메인 페이지
 from datetime import datetime
 
@@ -54,8 +57,11 @@ def today() :
     # 토큰 여부 확인
     result = tokenCheck()
     if result == False :
-        return redirect('http://localhost:5000/login')
+        return redirect('http://localhost:' + str(PORT) + '/login')
     print(result)
+
+
+    ######### 금일 메뉴 데이터
 
     # 시스템 날짜 가져와 년-월-일 출력
     # 이후 날짜 비교를 위해 date 변수 그대로 사용
@@ -86,10 +92,32 @@ def today() :
     # E-스퀘어 메뉴
     esq_menu = "라면, 김밥, 덮밥, 국수, 돈까스, 아이스크림"
 
+
+    ######### 이름판 데이터 
+    
+    # 경기드림타워 이름판
+    dtpeople = db.log.find({"place": "경기드림타워", "lunch": True, "date": date})
+    dtlist = []
+    for p in dtpeople:
+        dtlist.append(p['name'])
+    
+    # 경슐랭 이름판
+    kclpeople = db.log.find({"place": "경슐랭", "lunch": True, "date": date})
+    kcllist = []
+    for p in kclpeople:
+        kcllist.append(p['name'])
+
+    # 이스퀘어 이름판
+    esqpeople = db.log.find({"place": "이스퀘어", "lunch": True, "date": date})
+    esqlist = []
+    for p in esqpeople:
+        esqlist.append(p['name'])
+
     return render_template('today.html', 
                            template_date = daydate,
                            template_dt_lunch_menu = dt_l_menu, template_dt_dinner_menu = dt_d_menu,
-                           template_kcl_menu = kcl_menu, template_esq_menu = esq_menu)
+                           template_kcl_menu = kcl_menu, template_esq_menu = esq_menu,
+                           template_dtpeople = dtlist, template_kclpeople = kcllist, template_esqpeople = esqlist)
 
 
 #################################### 회원가입 
@@ -171,4 +199,4 @@ def loginPost() :
 #####################################
 
 if __name__ == "__main__" :
-    app.run("0.0.0.0", port=5001, debug=True)
+    app.run("0.0.0.0", port=PORT, debug=True)
